0a7e810affffea997878ccf839651973722aa64d
AAYUSH ANAND SINHA <98117714+Aayushsinha09@users.noreply.github.com>
2023-05-15 11:29:52 +0530

    Add files via upload

---
+++ to print an integer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int number;
+ 
+ printf("Enter an integer:");
+
+ // read and stores input 
+ scanf("%d", &number);
+
+ // displays output 
+ printf("you entered: %d", number);
+
+  return 0;
+}
